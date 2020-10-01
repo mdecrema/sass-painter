4381fe729cad7608d3956d50df1d9d81c9db4886
marco <marcodecrema@libero.it>
2020-10-01 15:37:39 +0200

    sezioni dei contenuti

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('src/app.js', 'dist/').sass('src/app.scss', 'dist/').options({
-       processCssUrls: false;
+       processCssUrls: false
    });
 
 
